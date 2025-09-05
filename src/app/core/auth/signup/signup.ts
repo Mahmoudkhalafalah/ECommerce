4f5e3140cb803e2228727f1a55b391b096d5d115
Mahmoud Khalafallah <mahmoudhamoda13579@gmail.com>
2025-09-05 15:28:59 +0300

    fix: Sign up feature: fixed repeat password and phone validation (2025-09-05)

--- src/app/core/auth/signup/signup.ts
+++ src/app/core/auth/signup/signup.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators,ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+  AbstractControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -8,11 +14,33 @@ import { FormControl, FormGroup, Validators,ReactiveFormsModule } from '@angular
   styleUrl: './signup.css',
 })
 export class Signup {
-  registerForm = new FormGroup({
-    name: new FormControl('',[Validators.required, Validators.minLength(10), Validators.maxLength(20)]),
-    email: new FormControl('',[Validators.required,Validators.email]),
-    password: new FormControl('',[Validators.required,Validators.minLength(8),Validators.maxLength(20)]),
-    rePassword: new FormControl('',[Validators.required,Validators.minLength(8),Validators.maxLength(20)]),
-    phone: new FormControl('',[Validators.required,Validators.pattern(/^01$/)])
-  });
+  registerForm = new FormGroup(
+    {
+      name: new FormControl('', [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(20),
+      ]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(20),
+      ]),
+      rePassword: new FormControl('', [
+        Validators.required,
+      ]),
+      phone: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^01[0125][0-9]{8}$/),
+      ]),
+    },
+    { validators: this.confirmPassword }
+  );
+
+  confirmPassword(group: AbstractControl) {
+    return group.get('password')?.value === group.get('rePassword')?.value
+      ? null
+      : { mismatch: true };
+  }
 }
