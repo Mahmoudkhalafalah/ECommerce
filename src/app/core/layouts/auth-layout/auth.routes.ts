4aec6713374ac2dfe71cd49d959841d83b8a9e36
PC <m.mohamedkhalaf240@gmail.com>
2025-09-07 00:32:18 +0300

    UI adjustments

--- src/app/core/layouts/auth-layout/auth.routes.ts
+++ src/app/core/layouts/auth-layout/auth.routes.ts
@@ -1,12 +1,7 @@
 import { Routes } from '@angular/router';
-import path from 'path';
 
 export const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
-  },{
   path: 'login',
   loadComponent: () => import('./../../auth/login/login').then(m => m.Login),
  },
