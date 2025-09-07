b540863e794765bd4dc5bfe97b783a86f7bebf4a
PC <m.mohamedkhalaf240@gmail.com>
2025-09-07 03:50:32 +0300

    feat: Added Sign out button

--- src/app/core/auth/services/auth.ts
+++ src/app/core/auth/services/auth.ts
@@ -1,21 +1,26 @@
-import { Login } from './../login/login';
+import { CookieService } from 'ngx-cookie-service';
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment.development';
-import { UserModel } from '../../models/user';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private readonly httpClient = inject(HttpClient);
-
+  private readonly cookieService = inject(CookieService);
+  private readonly router = inject(Router);
   registerUser(user : object) : Observable<any> {
     return this.httpClient.post(environment.baseUrl + 'auth/signup', user);
   }
 
   loginUser(user : object) : Observable<any> {
     return this.httpClient.post(environment.baseUrl + 'auth/signin', user);
+  }
+  signOut() {
+    this.cookieService.delete('token');
+    this.router.navigate(['/login']);
   }
 }
--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../../core/auth/services/auth';
 
 @Component({
   selector: 'app-navbar',
@@ -9,4 +10,9 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 })
 export class Navbar {
   @Input() isLoggedIn: boolean = false;
+  private readonly authService = inject(AuthService);
+  logout() {
+    this.authService.signOut();
+    // Implement logout functionality here
+  }
 }
